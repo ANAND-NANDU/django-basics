6cb6edddd3ac74b4c7781b358df90cea2d8decc4
anand <anandprasad562@gmail.com>
2021-07-27 18:38:12 +0530

    added cars model to cars app

--- cars/views.py
+++ cars/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from . import models
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Welcome to cars app")
 
+	cars = models.cars.objects.all()
+	if(cars.exists() == False):
+		a=models.cars(name="msutang",company="ford").save()
+		print("creating mock data")
+	else:
+		print("data exists")
+	
+	return render(request,"cars/index.html")
 
-	
-	
